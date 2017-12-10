57d122608a6a755a2e3e6634696330966555baaa
Taylor Wong <trwong93@gmail.com>
2017-12-09 18:35:25 -0800

    Added home landing page for mobile

--- mobile/App.js
+++ mobile/App.js
@@ -2,45 +2,47 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput , Alert, Button, ListView, ActivityIndicator} from 'react-native';
 
 export default class App extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      text: "",
-      isLoading: true
+      eventTag: "",
     };
   }
 
-  componentDidMount(){
-    return fetch("https://code-ninjas.herokuapp.com/api/level_sets")
-    .then(response => response.json())
-    .then(responseJson => {
-      let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.setState({data: ds.cloneWithRows(responseJson.by_id), isLoading: false});
-    });
+  // componentDidMount(){
+  //   return fetch("https://code-ninjas.herokuapp.com/api/level_sets")
+  //   .then(response => response.json())
+  //   .then(responseJson => {
+  //     let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  //     this.setState({data: ds.cloneWithRows(responseJson.by_id), isLoading: false});
+  //   });
+  // }
+
+  findEventFromInput() {
+    
   }
+
   render() {
-    if (this.state.isLoading){
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    } else {
-      return (
-        <View style = {styles.container}>
-          <ListView dataSource = {this.state.data}
-            renderRow = {(level) => <Text>{level.name}</Text>}
-          />
-        </View>
-      );
-    }
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TextInput 
+          style={{ height: 40, }}
+          placeholder="Event Tag"
+          onChangeText={ eventTag => this.setState({eventTag})}
+        />
+        <Button
+          onPress={ () => this.findEventFromInput(this.state.eventTag) }
+          title="Next"
+        />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f00',
+    backgroundColor: '#000',
     flexDirection: "column"
   },
   photo: {
