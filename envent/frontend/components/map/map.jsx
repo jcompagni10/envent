fe11513d1bf1af6c035d17d026886545fa2e7432
Edward Bai <edwardbai31@gmail.com>
2017-12-11 22:57:44 -0800

    Initial map frontend commit

---
+++ envent/frontend/components/map/map.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Map extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
