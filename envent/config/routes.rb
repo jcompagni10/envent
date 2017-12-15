56f86e37a153a6aab7419b015060a965fa4c9d8a
Edward Bai <edwardbai31@gmail.com>
2017-12-14 18:46:22 -0800

    Add routes changes

--- envent/config/routes.rb
+++ envent/config/routes.rb
@@ -7,22 +7,16 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :create]
     resources :events, only: [:index, :show, :create] do
       resources :schedule_items, only: [:index, :create, :update]
-<<<<<<< HEAD
       resources :maps, only: [:show, :destroy]
-=======
       resources :news, only: [:create, :index]
       resources :info, only: [:create, :show, :update]
       resources :messages, only: [:create, :index]
->>>>>>> 65e2843df361dd5f63a5dd35586974babe31ce42
     end
     resources :messages, only: [:show, :update, :delete]
     resources :news, only: [:show, :destroy, :update]
     resources :schedule_items, only: [:show, :destroy]
     resources :maps, only: [:create, :show, :destroy]
-<<<<<<< HEAD
     resources :news, only: [:create, :index, :update]
-=======
->>>>>>> 65e2843df361dd5f63a5dd35586974babe31ce42
     get 'events/show_id/:id', :to => 'events#show_id'
   end
 end
