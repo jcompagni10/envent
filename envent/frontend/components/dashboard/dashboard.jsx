6492cfaec3e491cafde1de7ef670bb8dae0ec993
Julian Compagni Portis <jcompagni@gmail.com>
2017-12-17 00:24:20 -0800

    dashboard style

--- envent/frontend/components/dashboard/dashboard.jsx
+++ envent/frontend/components/dashboard/dashboard.jsx
@@ -3,12 +3,30 @@ import {Link} from 'react-router-dom';
 import EventIndexContainer from './../event/event_index_container';
 import {Glyphicon} from 'react-bootstrap';
 
+const dataItems = [
+  {label: "All Events", icon: "home"},
+  {label: "Current Events", icon: "home"},
+  {label: "Total Views", icon: "home"},
+  {label: "Avg. Views", icon: "home"},
+];
 export default()=>{
   return (
       <div className="dashboard-container">
+
+        // datas
+        // total events, upcoming events, current_events
         <div className="dashboard-header-container">
           <div className="dashboard-header">
             Dashboard
+          </div>
+          <div className = "dashboard-data-container">
+            {dataItems.map(item=>(
+              <div className="dashboard-data-item">
+                <div className="icon">
+                  <Glyphicon glyph = {item.icon}/>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
         <div className="dashboard-content">
